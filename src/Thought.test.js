9d102107a589a1e2054ea0778a800f0e8237ced5
HasRule <hasrulwho@gmail.com>
2024-08-21 13:34:25 +0200

    first commit

---
+++ src/Thought.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Thought } from "./Thought.js";
+
+// Import render and screen here
+
+it("Displays the Thought component", () => {
+  // Pass to Thought component as thought prop
+  const thought = { text: "learn react testing library" };
+  // Add your testing logic here
+  
+});
