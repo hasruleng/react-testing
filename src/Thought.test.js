910a350a80fb181db6c08ff75ae5c6ab6c8c8c4f
HasRule <hasrulwho@gmail.com>
2024-08-23 11:53:18 +0200

    first pass test

--- src/Thought.test.js
+++ src/Thought.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Thought } from "./Thought.js";
+import { render, screen } from '@testing-library/react'
+
 
 // Import render and screen here
 
@@ -7,5 +9,9 @@ it("Displays the Thought component", () => {
   // Pass to Thought component as thought prop
   const thought = { text: "learn react testing library" };
   // Add your testing logic here
-  
+  render(<Thought 
+    thought = { thought }
+    removeThought = { () => {} }
+  />);
+  screen.debug()
 });
